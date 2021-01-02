f7a5d7d79315a1199b97f1b337ce8dcc2b16e041
Finleyyy <prinzi183@gmail.com>
2021-01-02 12:49:34 +0100

    initial commit

---
+++ cogs/mal.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+
+class MALCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='mal')
+    async def mal(self, ctx, arg="Davolaf"):
+        malUrl = 'https://myanimelist.net/animelist/' + arg
+        await ctx.send(arg + '\'s MAL: \n' + malUrl)
+
+    @commands.command(name='test')
+    async def test(self, ctx, arg="Davolaf"):
+        embed = discord.Embed(title="MyAnimeList", color=0x2e51a2)
+        embed.set_thumbnail(url="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png")
+        embed.add_field(name=arg + "\'s MAL:", value="https://myanimelist.net/animelist/" + arg, inline=True)
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(MALCog(bot))
---
+++ davobot.py
@@ -0,0 +1,42 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='!')
+initial_extensions = ['cogs.mal']
+
+bot.remove_command('mal')
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="anime"))
+    botUser = bot.user.name + '#' + bot.user.discriminator
+    print('Loged in as ' + botUser)
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.lower().__contains__('davobot'):
+        emoji = '👋'
+        await message.add_reaction(emoji)
+    await bot.process_commands(message)
+
+
+@bot.command(name='ping', description="Returns the bots latency")
+async def ping(ctx):
+    await ctx.send('Pong! {0} seconds latency'.format(round(bot.latency, 3)))
+
+
+print("Bot is ready!")
+bot.run(TOKEN)
